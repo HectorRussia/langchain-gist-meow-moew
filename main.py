49bc6698bd55442fe2402877e0456a29538a25de
Ponkrit <ponkrit.wo@hotmail.com>
2025-09-26 17:21:46 +0700

    feat: add get_text_length function to calculate text length

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+
+def get_text_lenght(text : str) -> int : 
+    
+    print(f"get_text_length enter with {text = }")
+
+    text = text.strip("'\n").strip('"')
+
+    return len(text)
+
+
+
 def main():
     print("Hello from langchain-gist-meow-meow!")
 
